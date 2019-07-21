9ff381992f4686763840a56493e394fce8106ac2
Robson <r.ramoscoutinho@gmail.com>
2019-07-21 18:17:40 -0300

    Introducion with Spring Shell

---
+++ src/main/java/br/com/robsonc/springCli/SpringCliApplication.java
@@ -0,0 +1,13 @@
+package br.com.robsonc.springCli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCliApplication.class, args);
+	}
+
+}
